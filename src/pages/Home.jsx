308ea0a9e72952f1094d569a368683d11e2a88b6
AbrahanTV <abrahantv@icloud.com>
2025-10-10 10:35:48 -0400

    changed img src

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,10 @@ const Home = () => {
         <div className="container d-flex justify-content-center align-items-center gap-4 my-5">
           <div className="card-cont d-flex justify-content-center align-items-center gap-4">
             <div className="card d-flex justify-content-center align-items-center">
-              <img src="/public/img/cat-01.jpg" alt="cat 1" />
+              <img
+                src={`${import.meta.env.BASE_URL}img/cat-01.jpg`}
+                alt="cat 1"
+              />
             </div>
             <div className="card d-flex justify-content-center align-items-center">
               <img src="" alt="" />
